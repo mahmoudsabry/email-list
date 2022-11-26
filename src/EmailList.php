bd87c666c4eb2d6cf8b557fe29d162e4016a544d
Mahmoud Sabry <mhsabry@yahoo.com>
2022-11-27 00:15:48 +0200

    init

---
+++ src/EmailList.php
@@ -0,0 +1,11 @@
+<?php
+namespace MS\EmailList;
+
+class EmailList
+{
+
+    function getName(){
+        return 'EmailList';
+    }
+
+}
---
+++ src/EmailListServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace MS\EmailList;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmailList extends ServiceProvider
+{
+ /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
+
